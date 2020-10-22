9ac6e6b4a3c49a769aa7152579038204063e0f2c
Sairsey <volodya_parus@mail.ru>
2020-10-22 20:20:32 +0300

    Minor changes

--- Task1/service1.py
+++ Task1/service1.py
@@ -1,6 +1,7 @@
 import cv2
 from multiprocessing import Process, Manager, Value
-import json
+import queue
+import yaml
 import pika
 import pickle
 
@@ -8,46 +9,43 @@ def grabber(path, q, n):
     with n.get_lock():
         n.value += 1
     cap = cv2.VideoCapture(path)
-    i = 0
     while (cap.isOpened()):
         ret, frame = cap.read()
         if (ret != True):
             break
-#        print("Grabbed frame")
         q.put(frame)
-#        if i > 20:
-#            break
-#        else:
-#            i += 1
     with n.get_lock():
         n.value -= 1
     print("grabber quitted")
 
 def resizer(q_in, q_out, size, n_grabbers):
-    while(n_grabbers.value > 0):
-        if (not q_in.empty()):
-            frame = q_in.get()
-            newframe = cv2.resize(frame, size)
- #           print("Frame resized")
-            q_out.put(newframe)
+    while n_grabbers.value > 0:
+        try:
+            frame = q_in.get_nowait() 
+        except queue.Empty:
+            continue            
+        newframe = cv2.resize(frame, size)
+        q_out.put(newframe)
     print("resizer quitted")
 
 def rabitter(q_in, n_grabbers):
-    while(n_grabbers.value > 0):
-        if (not q_in.empty()):
-            connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-            channel = connection.channel()
-            channel.queue_declare(queue='frames')
-            frame = q_in.get()
-#            print("Publish frame")
-            channel.basic_publish(exchange='', routing_key='frames', body=pickle.dumps(frame))
-            connection.close()
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue='frames')
+    while n_grabbers.value > 0:
+        try:
+            frame = q_in.get_nowait()
+        except queue.Empty:
+            continue                        
+        channel.basic_publish(exchange='', routing_key='frames', body=pickle.dumps(frame))
+            
     print("rabbiter quitted")
+    connection.close()
 
 if __name__ == '__main__':
     config = []
-    with open("config.json") as f:
-        config = json.load(f)
+    with open("config.yaml") as f:
+        config = yaml.load(f)
 
     with Manager() as manager:
         Queue1 = manager.Queue()
